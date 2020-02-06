7ecbccdd2bf59e83b72751e58ced4ec5ce0b23c4
carsendecker <cad631@nyu.edu>
2020-02-06 15:14:55 -0500

    Moved some stuff around, now the player states are easier to find and edit

---
+++ Assets/Scripts/Player/PlayerPhase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THIS IS FOR THE PLAYER STATE MACHINE.
+
+/// <summary>
+/// The abstract class to be used in the player state machine.
+/// </summary>
+public abstract class PlayerPhase
+{
+	public PlayerController player;
+	
+	/// <summary>
+	/// Gets called whenever the player enters the state
+	/// </summary>
+	public abstract void OnEnter();
+	
+	/// <summary>
+	/// Gets called every frame while the player is in the state (like Update())
+	/// </summary>
+	public abstract void Run();
+	
+	/// <summary>
+	/// Gets called whenever the player exits the state
+	/// </summary>
+	public abstract void OnExit();
+
+	
+	public virtual void OnCollisionEnter2D(Collision2D col)
+	{
+	}
+
+	public virtual void OnTriggerEnter2D(Collider2D col)
+	{
+	}
+
+	public virtual void OnTriggerExit2D(Collider2D col)
+	{
+	}
+}
