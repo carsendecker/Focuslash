ab340a0b67e59c819834ddbab33be238a94e6af5
Genric <gj.elazegui@Gmail.com>
2020-02-18 16:21:05 -0500

    Health Upgrade Script is in. Right now, it needs to update the current health so that it is a lot more accurate.

---
+++ Assets/Scripts/HealthUpgrade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUpgrade : MonoBehaviour
+{
+    public int healthIncreaseNumber;
+    public void AddMoreHealth(int healthToAdd)
+    {
+        Services.Player.MaxHealth += healthToAdd;
+        //Services.Player.health += healthToAdd;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AddMoreHealth(healthIncreaseNumber);
+            Services.UI.UpdatePlayerHealth();
+            
+            Destroy(this.gameObject);
+            Debug.Log(Services.Player.GetHealth()); 
+        }
+    }
+
+}
