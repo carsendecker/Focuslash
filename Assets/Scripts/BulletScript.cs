a9b28f89a12847525ee0f9e72904a97c85d21903
carsendecker <cad631@nyu.edu>
2020-03-05 16:24:30 -0500

    slinger

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float MoveSpeed;
+    public int Damage;
+
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = transform.right * MoveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !Services.Player.IsPhase(PlayerController.Phase.Attacking))
+        {
+            Services.Player.TakeDamage(Damage);
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
