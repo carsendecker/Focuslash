dae3867e317f12f06f0bb4ae3c53f8f8fc14452c
carsendecker <cad631@nyu.edu>
2020-03-24 16:14:30 -0700

    tutorial stuff and focus puzzles

---
+++ Assets/Scripts/World/SlashPuzzle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class SlashPuzzle : MonoBehaviour
+{
+    [HideInInspector] public List<SlashSensor> SlashSensors = new List<SlashSensor>();
+    public GameObject DoorToOpen;
+
+    public bool Opened
+    {
+        get { return isOpened; }
+        set
+        {
+            isOpened = value;
+            if (value)
+            {
+                Destroy(DoorToOpen.gameObject);
+                Services.Events.Unregister<PlayerLeftAttackPhase>(ResetSensors);
+            }
+        }
+    }
+    private bool isOpened;
+
+    
+    
+    void Start()
+    {
+        Services.Events.Register<PlayerLeftAttackPhase>(ResetSensors);
+    }
+
+    void ResetSensors(AGPEvent e)
+    {
+        byte sensorsActive = 0;
+        foreach (SlashSensor sensor in SlashSensors)
+        {
+            if (sensor.Active) sensorsActive++;
+        }
+
+        if (sensorsActive >= SlashSensors.Count)
+            Opened = true;
+        
+        if (!Opened)
+        {
+            foreach (SlashSensor sensor in SlashSensors)
+            {
+                sensor.Active = false;
+            }
+        }
+        
+    }
+}
