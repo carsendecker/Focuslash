6cd3cb62be4f0be574808efa491b5f12b698b6f4
carsendecker <cad631@nyu.edu>
2020-04-13 20:37:38 -0700

    part of special bullet attack system

---
+++ Assets/Scripts/ScriptableObjects/Attack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+[CreateAssetMenu(fileName = "BulletAttack", menuName = "Creatures/BulletAttack")]
+public class BulletAttack : ScriptableObject
+{
+    // [System.Serializable]
+    // public struct Direction
+    // {
+    //     public bool up, down, left, right, upLeft, upRight, downLeft, downRight;
+    //     [Space(10)]
+    //     public bool all;
+    //     public bool atPlayer;
+    // }
+    
+    public enum FireDirection
+
+    [System.Serializable]
+    public struct BulletPattern
+    {
+        
+    }
+
+    public GameObject BulletPrefab;
+    public int damagePerBullet;
+    
+    [Tooltip("Bullets fired per second, per emitter")]
+    public int fireRate;
+
+    [Tooltip("The directions that bullets will shoot in at once.")]
+    public Directions directionsToShoot;
+}
