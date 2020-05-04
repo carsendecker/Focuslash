960f56b7a91dd4669b67e4a9c39a299f29e2dacf
Carsen Decker <cad631@nyu.edu>
2020-05-04 02:26:11 -0700

    some testing with task functions and such

--- Assets/Scripts/Creatures/Boss/BossScript.cs
+++ Assets/Scripts/Creatures/Boss/BossScript.cs
@@ -12,26 +12,57 @@ public class BossScript : Enemy
 
     void Start()
     {
-        
+        var tester = new DelegateTask(FireInit, delegate { return FireBulletAttack(BulletAttacks[0]); });
+        taskManager.Do(tester);
     }
 
     void Update()
     {
-        
+        taskManager.Update();
+    }
+
+    /// <summary>
+    /// Initialization for firing a bullet attack
+    /// </summary>
+    private void FireInit()
+    {
+        Debug.Log("Initialized!");
+        currentAttack = 0;
+        duration = 0;
     }
 
     /// <summary>
     /// Takes a BulletAttack object and fires the attack according to them specific specifications
     /// </summary>
+    private float duration;
     private bool FireBulletAttack(BulletAttack attack)
     {
+        if (duration >= attack.AttackDuration)
+        {
+            return true;
+        }
         
+        
+
+        duration += Time.deltaTime;
+
         return false;
     }
 
-    private void FireComboAttack(params BulletAttack[] attacks)
+    /// <summary>
+    /// fires multiple attacks in succession
+    /// </summary>
+    int currentAttack = 0;
+    private bool FireComboAttack(params BulletAttack[] attacks)
     {
-        
+
+        if (currentAttack == attacks.Length)
+            return true;
+
+        if (FireBulletAttack(attacks[currentAttack]))
+            currentAttack++;
+
+        return false;
     }
 
     private class CompoundAttack : Task
