0aeac40a2085a4215e65cd847554e56a4324e44f
Carsen Decker <cad631@nyu.edu>
2020-05-03 22:38:56 -0700

    boss stuff

---
+++ Assets/Scripts/Creatures/Boss/BossScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : Enemy
+{
+    public List<BulletAttack> BulletAttacks = new List<BulletAttack>();
+    
+    
+    private TaskManager taskManager = new TaskManager();
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private class BasicAttack : Task
+    {
+        private BossScript boss;
+        private BulletAttack attack;
+        
+        public BasicAttack(BossScript boss, BulletAttack attack)
+        {
+            this.boss = boss;
+            this.attack = attack;
+        }
+    }
+}
